fec6d5cf2fce83d2784fe0dd140218a80ef2f9c9
danilojpfreitas <danilojpfreitas@gmail.com>
2022-09-21 17:23:46 -0300

    Add: Event

--- src/database/datasourse.ts
+++ src/database/datasourse.ts
@@ -7,7 +7,7 @@ const port = process.env.DB_PORT as number | undefined
 export const AppDataSource = new DataSource({
     type: 'mysql',
     host: process.env.DB_HOST,
-    port: 3306,
+    port: port,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
