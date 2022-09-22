93ec71b278c5ae16145d8e6a145ff5a65825117b
Fabricio <fabriciosilvateixeira@hotmail.com>
2022-09-21 22:17:02 -0300

    update: adds quotation entity and starting quotation controller

---
+++ src/entities/Quotation.ts
@@ -0,0 +1,38 @@
+import { Event } from './Event';
+import { Column, Entity,PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, Unique, ManyToOne, JoinColumn } from "typeorm";
+import { Length } from "class-validator";
+
+@Entity("quotation")
+export class Quotation{
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    @Length(1, 50)
+    description: string;
+
+    @Column()
+    provider: string;
+
+    @Column()
+    expected_expense: number;
+
+    @Column()
+    actual_expense: number;
+
+    @Column()
+    amount_already_paid: number;
+
+    @Column()
+    @CreateDateColumn()
+    createDateColumn: Date;
+
+    @Column()
+    @UpdateDateColumn()
+    updateDateColumn: Date;
+
+    @ManyToOne(()=> Event, (event) => event.quotation)
+   
+    @JoinColumn({ name :"event_id"})
+    event: Event
+};
