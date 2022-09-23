513084f397ca34eadf975954f5ac243db6fc5088
danilojpfreitas <danilojpfreitas@gmail.com>
2022-09-22 22:32:34 -0300

    Add: event routes

--- src/controllers/EventController.ts
+++ src/controllers/EventController.ts
@@ -7,10 +7,11 @@ import { EntityNotFoundError } from "typeorm";
 export class EventController {
 
   static async createEvent(req: Request, res: Response) {
-    const {place, name, date} = req.body;
+    const {idUser, place, name, date} = req.body;
 
     let event: Event
     const newEvent = eventRepository.create({
+      idUser,
       place,
       name,
       date,
@@ -28,4 +29,35 @@ export class EventController {
     return res.status(201).json(newEvent);
   }
 
+  static async getAllEvents(req: Request, res: Response) {
+    let allEvents: Array<Event> = [];
+    try {
+      allEvents = await eventRepository.find()
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send("Internal Server Error");
+    }
+
+    return res.status(200).send(allEvents);
+  }
+
+  static async getEventbyIdUser(req: Request, res: Response) {
+    const idUser = req.params.idUser;
+    let event: Event;
+
+    try {
+      event = await eventRepository.findOneOrFail({
+        where: { id: Number(idUser)},
+      });
+    } catch (error) {
+      if (error instanceof EntityNotFoundError) {
+        return res.status(404).send("User not found");
+      }
+      return res.status(500).json(error);
+    }
+
+    return res.send(event)
+  }
+
+  // static async editUser(req: Request, res: Response)
 }
--- src/routes/event.ts
+++ src/routes/event.ts
@@ -4,5 +4,12 @@ import { Router } from "express";
 const router = Router();
 
 router.post("/event", EventController.createEvent)
+router.get("/event", EventController.getAllEvents)
+router.get("/event/:idUser([0-9]+)", EventController.getEventbyIdUser)
+router.put("/event", )
+router.delete("/event", )
+
+router.get("/event/allexpected_expense", )
+router.get("/event/allexpense", )
 
 export default router;
