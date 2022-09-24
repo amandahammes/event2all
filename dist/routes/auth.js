f5e94391791f22fdaa00d7630a103ab13f851318
danilojpfreitas <danilojpfreitas@gmail.com>
2022-09-24 09:45:45 -0300

    Remove: /dist

--- dist/routes/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const AuthController_1 = require("../controllers/AuthController");
-const express_1 = require("express");
-// import {checkJwt} from "../middlewares/checkJwt";
-const router = (0, express_1.Router)();
-router.post("/login", AuthController_1.AuthController.auth);
-router.put("/change-password", AuthController_1.AuthController.changePassword);
-exports.default = router;
