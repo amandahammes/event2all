2116ceffe49b4648e925c7c61fd98bc445c09362
danilojpfreitas <danilojpfreitas@gmail.com>
2022-09-26 20:34:57 -0300

    Remove: dist

--- dist/repositories/guestRepository.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.guestRepository = void 0;
-const datasource_1 = require("../datasource");
-const Guest_1 = require("../entities/Guest");
-exports.guestRepository = datasource_1.AppDataSource.getRepository(Guest_1.Guest);
