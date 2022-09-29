a7b6f4a9aaf573deaa618a2517d0751cff9b276e
Giordano Cassini <giordano.cassini@gmail.com>
2022-09-28 22:12:31 -0300

    refactor: added app file due to be able to test

--- src/datasource.ts
+++ src/datasource.ts
@@ -5,20 +5,6 @@ import { DataSource, DataSourceOptions } from "typeorm";
 const port = process.env.DB_PORT as number | undefined;
 
 const options: DataSourceOptions = {
-<<<<<<< HEAD
-  type: "mysql",
-  host: process.env.DB_HOST,
-  port: port,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  synchronize: true,
-  logging: false,
-  entities: [`${__dirname}/**/entities/*.{ts,js}`],
-  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
-  // seeds: [MainSeeder]
-};
-=======
     type: 'mysql',
     host: process.env.DB_HOST,
     port: port,
@@ -30,6 +16,5 @@ const options: DataSourceOptions = {
     entities: [`${__dirname}/**/entities/*.{ts,js}`],
     migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
 }
->>>>>>> d0b30330b70f1de95405562c04ae84242bddeaa8
 
 export const AppDataSource = new DataSource(options);
