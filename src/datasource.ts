d4e6efc8b73eb6f0d32ee1b8b32f4127270b0bde
Giordano Cassini <giordano.cassini@gmail.com>
2022-09-28 22:05:51 -0300

    refactor

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,10 +1,10 @@
-import 'dotenv/config'
-import 'reflect-metadata'
+import "dotenv/config";
+import "reflect-metadata";
 import { DataSource, DataSourceOptions } from "typeorm";
 // import { SeederOptions } from "typeorm-extension"
 // import { MainSeeder } from './seeds/MainSeeder';
 
-const port = process.env.DB_PORT as number | undefined
+const port = process.env.DB_PORT as number | undefined;
 
 // const options: DataSourceOptions & SeederOptions = {
 //     type: 'mysql',
@@ -21,17 +21,17 @@ const port = process.env.DB_PORT as number | undefined
 // }
 
 const options: DataSourceOptions = {
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: port,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    synchronize: true,
-    logging: false,
-    entities: [`${__dirname}/**/entities/*.{ts,js}`],
-    migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
-    // seeds: [MainSeeder]
-}
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: port,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: false,
+  entities: [`${__dirname}/**/entities/*.{ts,js}`],
+  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+  // seeds: [MainSeeder]
+};
 
-export const AppDataSource = new DataSource(options)
+export const AppDataSource = new DataSource(options);
